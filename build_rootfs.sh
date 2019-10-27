cf706e77da974d22565abe92f93738bd0755cf58
11philip22 <philipwoldhek@gmail.com>
2019-10-27 12:53:45 +0100

    bla

--- build_rootfs.sh
+++ build_rootfs.sh
@@ -1,6 +1,5 @@
 tmp_dir=$(mkdir build_dir)
-env -i pacstrap -c -G -M ${tmp_dir}/ \
-    pacman systemd
+env -i pacstrap -c -G -M ${tmp_dir}/ pacman systemd
 
 cp --recursive --preserve=timestamps --backup --suffix=.pacnew rootfs/* ${tmp_dir}/
 arch-chroot ${tmp_dir} locale-gen
@@ -8,4 +7,4 @@ arch-chroot ${tmp_dir} pacman-key --init
 arch-chroot ${tmp_dir} pacman-key --populate archlinux
 
 tar --numeric-owner --xattrs --acls --exclude-from=exclude -C ${tmp_dir} -c . -f archlinux.tar
-rm -rf ${tmp_dir}
+rm -rf ${tmp_dir}   
